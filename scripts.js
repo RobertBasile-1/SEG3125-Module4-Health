5818f96e8bea32493e1ca002078e805c8772e023
Robert <rbasi008@uottawa.ca>
2021-06-06 00:07:11 -0400

    peep

--- scripts.js
+++ scripts.js
@@ -1,7 +1,23 @@
  $(document).ready(function() {
+     /* Credit for the if statement logic and animation to this website */
+     /*https://deepmikoto.com/coding/3--how-to-create-back-to-top-button-using-jquery-and-some-css*/
+     $(function() {
+         $(window).scroll(function() {
+             if ($(this).scrollTop() - 200 > 0) {
+                 $('#warning').stop().slideDown('fast'); // show the button
+             } else {
+                 $('#warning').stop().slideUp('fast'); // hide the button
+             }
+         });
+     });
 
+     $(function() {
+         // previous detection logic
 
-     $(window).on("load", function() {
-
+         $("#warning").on("click", function() {
+             $("html, body").animate({
+                 scrollTop: 0
+             }, 200);
+         });
      });
  });
